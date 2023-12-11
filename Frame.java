c3da0450231dab0da1890516475a8ca3f441f50c
Sebastian Sonne <152827009+Sebastian-Sonne@users.noreply.github.com>
2023-12-11 17:09:45 -0600

    Add files via upload

---
+++ Frame.java
@@ -0,0 +1,28 @@
+import javax.swing.JFrame;
+
+/**
+ * Class to setup the frame of a graphical output
+ * 
+ * @author Sebastian Sonne
+ * @version v2 | 25.06.23
+ */
+public class Frame extends JFrame{
+    
+    public static void main(String[] args) {
+        new Frame();
+    }
+    
+    /**
+     * constructor of Frame Class
+     */
+    public Frame()
+    {   
+        this.add(new Panel());
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setFocusable(false);
+        this.setVisible(true);
+        this.setTitle("Verschlüsselungsmaschine");
+    }
+}
